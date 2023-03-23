a30c3391b561b424ea2a4eee2cd15a9ce4c26a1a
FractosOiluj <jclmu_mu@hotmail.com>
2023-03-23 23:30:22 +0000

    novos itens

--- bot.py
+++ bot.py
@@ -2,28 +2,30 @@ import requests
 from bs4 import BeautifulSoup
 import time
 
-
-# ask me about the max value im willing to pay for renting a apt
-valorMaximo = input("how much is enough for paying for renting a apt: \n")
-print("Give me a number so i can send you the options")
-numero_whats = input("digite o numero no formato: +000.00.0000000. >")
-print("wait for the bot to search and gather the information")
-
-
+# define function that will be resposible for connecting with website via requests's get method,
+# and its going to instanciate it using BeautifulSoup(text_html,'lxml')
 
 def find_apts():
     # use get method from requests lib. it requests information from the website
     # # we are using '.txt' for requesting only text.
-    text_html = requests.get(f'https://www.queroarrendar.com/imoveis/apartamentos/arrendamento/portugal/?pricemin=0&pricemax={valorMaximo}').text
+    x = 1 
+    while x <= 11:
+        text_html = requests.get(f'https://www.queroarrendar.com/imoveis/apartamentos/arrendamento/porto/?page={x}').text
+        x+=1
+        
+        # create a beautiful instance. 'html_text' is the object we want to scrap
+        # lxml is th parse 'mode'
+        soup = BeautifulSoup(text_html, 'lxml')
 
-    # create a beautiful instance. 'html_text' is the object we want to scrap
-    # lxml is th parse 'mode'
-    soup = BeautifulSoup(text_html, 'lxml')
+        # now we select via 'find()' method in html the element we want to
+        # look apart which, in this case is 'xxx' tag 'zzzz' element
+        apts_price = soup.find_all('p', class_ = 'price')
+        apts_location = soup.find_all('p', class_ = 'location')
+        
 
-    # now we select via 'find()' method in html the element we want to
-    # look apart which, in this case is 'xxx' tag 'zzzz' element
-    apts = soup.find_all('item')
+        for apt in apts_price:
+            print(apt.text)
 
-    print(apts)    
-
-find_apts()  
+        for apt in apts_location:
+            print(apt.text)
+find_apts() 
